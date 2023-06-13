0dbf00594f1a17ca200ff35fb8595ab3bbd18d21
FRANZENR <FRANZENR@chop.edu>
2023-06-13 00:11:53 -0400

    Update check_versions.sh

--- assets/check_versions.sh
+++ assets/check_versions.sh
@@ -27,7 +27,7 @@ awk 'BEGIN { RS = "diff --git" }   # splits diff file such that each file repres
             #do nothing, as only modules need to be checked for version number incrementation. 
         }
     
-} ' $1 
+} ' "$1" 
 
 if [[ -s version_issues ]] # if file size > 0, ie there are issues with any of the modules
     then 
